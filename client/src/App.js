5094a6bbf73050e74b24ebb06b088ff8d780c42c
ceddah <cedanik1@gmail.com>
2022-12-06 18:03:46 +0100

    homepage widgets

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import { themeSettings } from "./theme";
 function App() {
   const mode = useSelector((state) => state.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const isAuth = Boolean(useSelector((state) => state.token));
   return (
     <div className="app">
       <BrowserRouter>
@@ -18,8 +19,11 @@ function App() {
           <CssBaseline />
           <Routes>
             <Route path="/" element={<LoginPage />} />
-            <Route path="/home" element={<HomePage />} />
-            <Route path="/profile/:userId" element={<ProfilePage />} />
+            <Route path="/home" element={isAuth ? <HomePage /> : <Navigate to="/login" />} />
+            <Route
+              path="/profile/:userId"
+              element={isAuth ? <ProfilePage /> : <Navigate to="/login" />}
+            />
           </Routes>
         </ThemeProvider>
       </BrowserRouter>
---
+++ client/src/scenes/widgets/MyPostWidget.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import {
+  EditOutlined,
+  DeleteOutlined,
+  AttachFileOutlined,
+  GifBoxOutlined,
+  ImageOutlined,
+  MicOutlined,
+  MoreHorizOutlined,
+} from "@mui/icons-material";
+import {
+  Box,
+  Divider,
+  Typography,
+  InputBase,
+  useTheme,
+  Button,
+  IconButton,
+  useMediaQuery,
+} from "@mui/material";
+import Dropzone from "react-dropzone";
+import FlexBetween from "components/FlexBetween";
+import UserImage from "components/UserImage";
+import WidgetWrapper from "components/WidgetWrapper";
+
+import { useSelector, useDispatch } from "react-redux";
+import { setPosts } from "state";
+
+const MyPostWidget = ({ picturePath }) => {
+  const dispatch = useDispatch();
+  const [isImage, setIsImage] = useState(false);
+  const [image, setImage] = useState(null);
+  const [post, setPost] = useState("");
+  const { palette } = useTheme();
+  const {
+    user: { _id },
+    token,
+  } = useSelector((state) => state);
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+  const mediumMain = palette.neutral.mediumMain;
+  const medium = palette.neutral.medium;
+
+  const handlePost = async () => {
+    const formData = new FormData();
+    formData.append("userId", _id);
+    formData.append("description", post);
+    if (image) {
+      formData.append("picture", image);
+      formData.append("picturePath", image.name);
+    }
+
+    const response = await fetch(`http://localhost:3001/posts`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      body: formData,
+    });
+    const posts = await response.json();
+    dispatch(setPosts({ posts }));
+    setImage(null);
+    setPost("");
+  };
+
+  return <div>MyPostWidget</div>;
+};
+
+export default MyPostWidget;
